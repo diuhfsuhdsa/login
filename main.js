00e30934d8b50d6245e66a2cca234a0c6dddda51
diuhfsuhdsa <114018471+diuhfsuhdsa@users.noreply.github.com>
2022-09-21 10:36:34 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+window.open("https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?access_type=offline&client_id=254945981659.apps.googleusercontent.com&include_granted_scopes=true&prompt=consent&redirect_uri=https%3A%2F%2Fstudio.code.org%2Fusers%2Fauth%2Fgoogle_oauth2%2Fcallback&response_type=code&scope=email%20profile&state=8179795f02e404bba63a10c9df034c8a2b3e7e1696f16ee3&flowName=GeneralOAuthFlow");
+alert(%22okay%22);
+$(div[data-item-index|=%270%27]).click();
